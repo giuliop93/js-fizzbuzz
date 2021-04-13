7c030c29e324d0215dc5c60bea938bebb7450e9e
Giulio_Pa <giuliop93@hotmail.it>
2021-04-13 09:46:09 +0200

    wip

---
+++ script.js
@@ -0,0 +1,22 @@
+var numeri = []
+
+for (var i = 0; i < 100; i++) {
+    console.log("ciao")
+    if()
+    //se multipli di 3 e 5, stampo FizzBuzz
+
+risultato = "FizzBuzz"
+}else if() {}
+// se multipli di 5, stampo Buzz
+
+risultato = "Buzz"
+}else if() {
+    se multipli di 3, stampo Fizz
+
+    risultato = "Fizz"
+}/* else {
+    //se non sono multipli, stampo il numero stesso
+console.log()
+}*/
+
+console.log(risultato)
